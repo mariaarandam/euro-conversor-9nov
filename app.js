b72b3ce17136969902e3a85e1967a415ca2a0fa6
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-11-09 12:16:59 +0100

    done

--- app.js
+++ app.js
@@ -9,12 +9,20 @@ createApp({
     const amount = ref("");
 
     const convertToDollars = computed(() => {
-      return amount.value; // TODO: Apartado 1. Modifica este return para realizar la conversión de euros a dolares
+      return amount.value * 1.07; 
+      // TODO: Apartado 1. Modifica este return para realizar la conversión de euros a dolares          
+    });
+
+    const convertToYens = computed(() => {
+      return amount.value * 161.53; 
+          
     });
 
     return {
       amount,
       convertToDollars,
+      convertToYens,
+      
     };
   },
 }).mount("#app");
